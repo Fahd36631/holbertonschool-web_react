59feb400f246a84a0d70129cdfee380b05fbc90b
Fahd36631 <Fahd36631@gmail.com>
2026-04-16 10:44:13 +0300

    feat(task_1): add StudentClass with constructor and class interfaces

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -15,8 +15,38 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
 const printTeacher: printTeacherFunction = (firstName, lastName) =>
   `${firstName.charAt(0)}. ${lastName}`;
+
+const StudentClass: StudentClassConstructor = class StudentClass
+  implements StudentClassInterface
+{
+  private firstName: string;
+
+  private lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+};
 
 const teacher3: Teacher = {
   firstName: 'John',
@@ -37,3 +67,7 @@ const director1: Directors = {
 console.log(teacher3);
 console.log(director1);
 console.log(printTeacher('John', 'Doe'));
+
+const student = new StudentClass('Jane', 'Doe');
+console.log(student.workOnHomework());
+console.log(student.displayName());

88370a33c3e8e791dea313445cb753c522bf5a06
Fahd36631 <Fahd36631@gmail.com>
2026-04-16 10:41:48 +0300

    fix task 1

---
+++ TypeScript/task_1/js/main.ts
@@ -0,0 +1,31 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [attribute: string]: any;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(teacher3);
+console.log(director1);
